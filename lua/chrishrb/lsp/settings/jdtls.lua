74eefe7e320ea638f2e211867a265efc9637b767
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2024-04-07 01:15:09 +0200

    set tabstop to 4 for java files

--- lua/chrishrb/lsp/settings/jdtls.lua
+++ lua/chrishrb/lsp/settings/jdtls.lua
@@ -2,10 +2,6 @@ if not require("nixCatsUtils").isNixCats then
 	return
 end
 
--- setup java lsp settings
-vim.opt_local.shiftwidth = 4
-vim.opt_local.tabstop = 4
-
 vim.cmd("command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)")
 vim.cmd("command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)")
 vim.cmd("command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()")
