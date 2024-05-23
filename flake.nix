90ae9711707a0bd077891ae79b5c810476ed010d
Christoph Herb <52382992+chrishrb@users.noreply.github.com>
2024-05-23 21:25:51 +0200

    disable lsp debug

--- flake.nix
+++ flake.nix
@@ -328,7 +328,7 @@
 
           # this does not have an associated category of plugins, 
           # but lua can still check for it
-          lspDebugMode = true;
+          lspDebugMode = false;
 
           # you could also pass something else:
           colorscheme = "catppuccin";
